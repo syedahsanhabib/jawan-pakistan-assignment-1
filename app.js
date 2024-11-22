2d63cc3295a45f064a6f9c54e9657dfe290d7919
ahsan habib <syedahsanhabib021@gmail.com>
2024-11-22 15:25:35 +0500

    Assignment 1 complete

---
+++ app.js
@@ -0,0 +1,151 @@
+// Question number 1
+
+let myName = "Syed Muhammad Ahsan Habib";
+let myEmail = "syedahsanhabib021@gmail.com";
+let myCity = "Karachi";
+let myEducation = "BSCS";
+let myOccupation = "development";
+let myPhoneNumber = "03112266978";
+let myInstituteName = "virtual University";
+
+document.write(`My name is : ${myName} <br> My email is : ${myEmail} <br> My city is : ${myCity} <br>
+     My education is : ${myEducation} <br> My occupation is : ${myOccupation} <br>
+     My phone number is : ${myPhoneNumber} <br> My institute name is : ${myInstituteName}`);
+
+
+
+// Question number 2 //
+
+
+//global//
+
+// var name = "Ahsan"
+// var me = "habib"
+
+
+// {
+//     var name = "Ahsan habib"
+//     console.log(name); 
+//     console.log(me); 
+// }
+// console.log(name);
+// console.log(me);
+
+
+//  let name = "Ahsan";
+// {
+//     let name = "Ahsan habib";
+//     console.log(name);
+// }
+// console.log(name)
+
+// const name = "Ahsan";
+// {
+//     const name = "Ahsan habib";
+//     console.log(name);
+// }
+
+// console.log(name)
+
+
+//function scope//
+
+// function name (){
+//     var name = "Ahsan habib";
+//     console.log(name); 
+// }
+// name()
+
+
+// function name (){
+//     let name = "Ahsan";
+//     console.log(name); 
+// }
+// name()
+
+
+// function name (){
+//     const name = " Syed Ahsan";
+//     console.log(name); 
+// }
+// name()
+
+
+
+// block scope//
+
+// var name = "ahsan"
+// {
+//      var name = " Syed Muhammad Ahsan Habib";
+//      console.log(name); 
+// }
+// console.log(name)
+
+// let name = "ahsan"
+// {
+//      let name = " Syed Muhammad Ahsan Habib";
+//      console.log(name); 
+// }
+// console.log(name)
+
+// const name = "Syed Muhammad Ahsan Habib"
+// {
+//      const name = "Ahsan";
+//      console.log(name); 
+// }
+// console.log(name)
+
+
+//reassgined//
+
+// var name = "ahsan"
+// name = "habib"
+
+// console.log(name)
+
+// let name = "ahsan"
+// name = "habib"
+
+// console.log(name)
+
+// const name = "ahsan"
+// name = "habib"
+
+// console.log(name)
+
+
+
+// Redeclared//
+
+// var name = "ahsan"
+// var name = "habib"
+
+// console.log(name)
+
+
+// let name = "ahsan"
+// let name = "habib"
+
+// console.log(name)
+
+// const name = "ahsan"
+// const name = "habib"
+
+// console.log(name)
+
+
+// hoisting//
+
+// console.log(name)
+// var name = "ahsan"
+
+
+// console.log(name)
+// let name = "ahsan"
+
+// console.log(name)
+// const name = "ahsan"
+
+
+
+
